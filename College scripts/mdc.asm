2bc7f094cbdc4283985e04b0b4c3b1cbcb032594
Luan Rockenbach <71732714+luanrockenbach@users.noreply.github.com>
2022-07-20 15:14:28 -0300

    Initial commit
    Assembly on Neander-X to find the GDC

---
+++ College scripts/mdc.asm
@@ -0,0 +1,17 @@
+;---------------------------------------------------
+; Programa:MDC
+; Autor:Luan Rockenbach, Maicon Dickow.
+; Data:20/07/2022
+;---------------------------------------------------
+X EQU 248
+Y EQU 249
+
+IN 0
+STA X
+NOP
+IN 0
+STA Y
+
+
+
+HLT
